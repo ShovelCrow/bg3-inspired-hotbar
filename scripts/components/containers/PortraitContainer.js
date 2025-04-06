e0d1b6c546a8363c7303f8641c9f0447034f4610
Dapoulp <elcatnet@gmail.com>
2025-04-06 17:23:06 +0200

    h

--- scripts/components/containers/PortraitContainer.js
+++ scripts/components/containers/PortraitContainer.js
@@ -9,9 +9,34 @@ export class PortraitContainer extends BG3Component {
         return ["bg3-portrait-container"]
     }
 
+    get actor() {
+        return ui.BG3HOTBAR.manager.actor;
+    }
+
+    get img() {
+        return this.actor.img;
+    }
+
+    get health() {
+        const hpValue = this.actor.system.attributes?.hp?.value || 0;
+        const hpMax = this.actor.system.attributes?.hp?.max || 1;
+        const hpPercent = Math.max(0, Math.min(100, (hpValue / hpMax) * 100));
+        const damagePercent = 100 - hpPercent;
+        const tempHp = this.actor.system.attributes?.hp?.temp || 0;
+        return {
+            current: hpValue,
+            max: hpMax,
+            percent: hpPercent,
+            damage: damagePercent,
+            temp: tempHp
+        }
+    }
+
     async getData() {
         return {
-            img: ui.BG3HOTBAR.manager.actor.img
+            img: this.img,
+            health: this.health,
+            opacity: 1
         };
     }
 }
