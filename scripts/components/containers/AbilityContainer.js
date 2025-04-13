4e42ed40412db96f460e59100e0a87c36c66763a
Dapoulp <elcatnet@gmail.com>
2025-04-13 16:13:32 +0200

    Get abilities & skills from DND5E Config

--- scripts/components/containers/AbilityContainer.js
+++ scripts/components/containers/AbilityContainer.js
@@ -14,38 +14,12 @@ export class AbilityContainer extends BG3Component {
         this.element.addEventListener('click', (event) => MenuContainer.toggle(this.getMenuData(), this, event));
     }
 
-    getSkillLabel(key) {
-        return {
-            "ath": "Athletics",
-            "acr": "Acrobatics",
-            "slt": "Sleight of Hand",
-            "ste": "Stealth",
-            "arc": "Arcana",
-            "his": "History",
-            "inv": "Investigation",
-            "nat": "Nature",
-            "rel": "Religion",
-            "ani": "Animal Handling",
-            "ins": "Insight",
-            "med": "Medicine",
-            "prc": "Perception",
-            "sur": "Survival",
-            "dec": "Deception",
-            "itm": "Intimidation",
-            "prf": "Performance",
-            "per": "Persuasion"
-        }[key];
+    get abilities() {
+        return CONFIG.DND5E.abilities;
     }
 
-    get abilities() {
-        return {
-            str: { label: "Strength", mod: this.getAbilityMod('str'), skills: this.getSkillMod(["ath"]) },
-            dex: { label: "Dexterity", mod: this.getAbilityMod('dex'), skills: this.getSkillMod(["acr", "slt", "ste"]) },
-            con: { label: "Constitution", mod: this.getAbilityMod('con'), skills: [] },
-            int: { label: "Intelligence", mod: this.getAbilityMod('int'), skills: this.getSkillMod(["arc", "his", "inv", "nat", "rel"]) },
-            wis: { label: "Wisdom", mod: this.getAbilityMod('wis'), skills: this.getSkillMod(["ani", "ins", "med", "prc", "sur"]) },
-            cha: { label: "Charisma", mod: this.getAbilityMod('cha'), skills: this.getSkillMod(["dec", "itm", "prf", "per"]) }
-        };
+    get skills() {
+        return CONFIG.DND5E.skills;
     }
 
     getAbilityMod(key) {
@@ -66,21 +40,23 @@ export class AbilityContainer extends BG3Component {
                 disadvantage: event.ctrlKey,
                 fastForward: event.shiftKey
             });
-            // this.element.querySelectorAll('.bg3-menu-container').forEach(e => e.classList.add('hidden'));
         } catch (error) {
             ui.notifications.error(`Error rolling ${parent.dataset.key.toUpperCase()} save. See console for details.`);
         }
     };
 
-    getSkillMod(keys) {
+    getSkillMod(key) {
         const skills = {};
-        keys.forEach(k => {
+        let count = 0;
+        Object.entries(this.skills).forEach(([k, v]) => {
+            if(v.ability !== key) return;
+            count++;
             const skill = this.actor.system.skills?.[k] || { proficient: false },
                 mod = skill.total,
                 modStr = mod >= 0 ? `+${mod}` : mod.toString();
-            skills[k] = {label:  this.getSkillLabel(k), icon: 'fas fa-dice-d20', value: modStr, style: skill.proficient === 1 ?  'color: #3498db' : '', click: this.skillRoll.bind(this)}
-        })
-        return skills;
+            skills[k] = {label:  v.label, icon: 'fas fa-dice-d20', value: modStr, style: skill.proficient === 1 ?  'color: #3498db' : '', click: this.skillRoll.bind(this)}
+        });
+        return count > 0 ? skills : null;
     };
 
     getMenuData() {
@@ -118,48 +94,41 @@ export class AbilityContainer extends BG3Component {
             }
         };
 
+        const tooltip = "Click to show ability checks and saving throws";
+
         return {
             position: 'top',
             event: 'click',
             name: 'baseMenu',
             keepOpen: true,
             closeParent: true,
-            buttons: {
-                str: { ...{label: "Strength", skills: this.getSkillMod(["ath"]), tooltip: "Click to show ability checks and saving throws"}, ...this.getAbilityMod('str'),
-                subMenu: [
-                    {position: 'topright', name: 'saveMenu', event: 'click', buttons: {checkSTR: {...{label: 'Check', icon: 'fas fa-dice-d20', click: checkRoll}, ...this.getAbilityMod('str')}, saveSTR: {...{label: 'Save', icon: 'fas fa-dice-d20', click: saveRoll}, ...this.getAbilityMod('str')}}},
-                    {position: 'topleft', name: 'skillMenu', event: 'click', buttons: this.getSkillMod(["ath"])}
-                ] },
-                dex: { ...{label: "Dexterity", skills: this.getSkillMod(["acr", "slt", "ste"]), tooltip: "Click to show ability checks and saving throws"}, ...this.getAbilityMod('dex'),
-                subMenu: [
-                    {position: 'topright', name: 'saveMenu', event: 'click', buttons: {checkDEX: {...{label: 'Check', icon: 'fas fa-dice-d20', click: checkRoll}, ...this.getAbilityMod('dex')}, saveDEX: {...{label: 'Save', icon: 'fas fa-dice-d20', click: saveRoll}, ...this.getAbilityMod('dex')}}},
-                    {position: 'topleft', name: 'skillMenu', event: 'click', buttons: this.getSkillMod(["acr", "slt", "ste"])}
-                ] },
-                con: { ...{label: "Constitution", skills: [], tooltip: "Click to show ability checks and saving throws"}, ...this.getAbilityMod('con'),
-                subMenu: [
-                    {position: 'topright', name: 'saveMenu', event: 'click', buttons: {checkCON: {...{label: 'Check', icon: 'fas fa-dice-d20', click: checkRoll}, ...this.getAbilityMod('con')}, saveCON: {...{label: 'Save', icon: 'fas fa-dice-d20', click: saveRoll}, ...this.getAbilityMod('con')}}},
-                    {position: 'topleft', name: 'skillMenu', event: 'click', buttons: null}
-                ] },
-                int: { ...{label: "Intelligence", skills: this.getSkillMod(["arc", "his", "inv", "nat", "rel"]), tooltip: "Click to show ability checks and saving throws"}, ...this.getAbilityMod('int'),
-                subMenu: [
-                    {position: 'topright', name: 'saveMenu', event: 'click', buttons: {checkINT: {...{label: 'Check', icon: 'fas fa-dice-d20', click: checkRoll}, ...this.getAbilityMod('int')}, saveINT: {...{label: 'Save', icon: 'fas fa-dice-d20', click: saveRoll}, ...this.getAbilityMod('int')}}},
-                    {position: 'topleft', name: 'skillMenu', event: 'click', buttons: this.getSkillMod(["arc", "his", "inv", "nat", "rel"])}
-                ] },
-                wis: { ...{label: "Wisdom", skills: this.getSkillMod(["ani", "ins", "med", "prc", "sur"]), tooltip: "Click to show ability checks and saving throws"}, ...this.getAbilityMod('wis'),
-                subMenu: [
-                    {position: 'topright', name: 'saveMenu', event: 'click', buttons: {checkWIS: {...{label: 'Check', icon: 'fas fa-dice-d20', click: checkRoll}, ...this.getAbilityMod('wis')}, saveWIS: {...{label: 'Save', icon: 'fas fa-dice-d20', click: saveRoll}, ...this.getAbilityMod('wis')}}},
-                    {position: 'topleft', name: 'skillMenu', event: 'click', buttons: this.getSkillMod(["ani", "ins", "med", "prc", "sur"])}
-                ] },
-                cha: { ...{label: "Charisma", skills: this.getSkillMod(["dec", "itm", "prf", "per"]), tooltip: "Click to show ability checks and saving throws"}, ...this.getAbilityMod('cha'),
-                subMenu: [
-                    {position: 'topright', name: 'saveMenu', event: 'click', buttons: {checkCHA: {...{label: 'Check', icon: 'fas fa-dice-d20', click: checkRoll}, ...this.getAbilityMod('cha')}, saveCHA: {...{label: 'Save', icon: 'fas fa-dice-d20', click: saveRoll}, ...this.getAbilityMod('cha')}}},
-                    {position: 'topleft', name: 'skillMenu', event: 'click', buttons: this.getSkillMod(["dec", "itm", "prf", "per"])}
-                ] }
-            }
+            buttons: (() => {
+                const btns = {};
+                for(const abl in this.abilities) {
+                    const abilityMod = this.getAbilityMod(abl);
+                    btns[abl] = {
+                        ...{
+                            label: this.abilities[abl].label,
+                            tooltip: tooltip
+                        },
+                        ...abilityMod,
+                        subMenu: [
+                            {
+                                position: 'topright', name: 'saveMenu', event: 'click', 
+                                buttons: {
+                                    [`check${abl.toUpperCase}`]: {...{label: 'Check', icon: 'fas fa-dice-d20', click: checkRoll}, ...abilityMod},
+                                    [`save${abl.toUpperCase}`]: {...{label: 'Save', icon: 'fas fa-dice-d20', click: saveRoll}, ...abilityMod}
+                                }
+                            },
+                            {
+                                position: 'topleft', name: 'skillMenu', event: 'click',
+                                buttons: this.getSkillMod(abl)
+                            }
+                        ]
+                    }
+                };
+                return btns;
+            })()
         };
     };
-
-    // async _renderInner() {
-    //     await super._renderInner();
-    // }
 }
