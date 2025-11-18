d035d84655761440ab54ed9ad13de24cda5b1c17
ShovelCrow <keviraptor@gmail.com>
2025-11-18 18:52:06 -0500

    fix: custom filter behavior on nonitem resources - adjust custom filter styling - custom filters now support custom icons

--- scripts/components/buttons/filterButton.js
+++ scripts/components/buttons/filterButton.js
@@ -36,7 +36,7 @@ export class FilterButton extends BG3Component {
                 desc = `<div class="custom-tooltip content"><h4 class="title" style="--data-color:${this.data.color}"><i class="fas ${this.data.symbol}"></i>${this.data.label}<i class="fas ${this.data.symbol}"></i></h4><p class="notes"><i>Left Click to highlight items using this resource.</i></p><p class="notes"><i>Right Click to grey out.</i></p></div>`; 
                 break;
             case 'feature':
-                desc = `<div class="custom-tooltip content"><h4 class="title" style="--data-color:${this.data.color}"><i class="fas ${this.data.symbol}"></i>Feature<i class="fas ${this.data.symbol}"></i></h4><p class="notes"><i>Left Click to highlight items of type feature.</i></p><p class="notes"><i>Right Click to grey out.</i></p></div>`; 
+                desc = `<div class="custom-tooltip content"><h4 class="title" style="--data-color:${this.data.color}"><i class="fas ${this.data.symbol}"></i>${game.i18n.localize("TYPES.Item.feat")}<i class="fas ${this.data.symbol}"></i></h4><p class="notes"><i>Left Click to highlight items of type feature.</i></p><p class="notes"><i>Right Click to grey out.</i></p></div>`; 
                 break;
             case 'spell':
                 const label = !this.data.isPact && !this.data.isApothecary && this.data.level > 0 ? `${this.data.label} ${this.data.level}` : this.data.label;
@@ -46,7 +46,11 @@ export class FilterButton extends BG3Component {
                 desc = this.data.custom?.tooltip ? `
                     <div class="custom-tooltip content">
                         <div class="header">
-                            <h4 class="title" style="--data-color:${this.data.color}">${this.data.custom?.tooltip?.label}</h4>
+                            <h4 class="title" style="--data-color:${this.data.color}">
+                                ${this.data.symbol ? `<i class="fas ${this.data.symbol}"></i>`: ''}
+                                ${this.data.custom?.tooltip?.label}
+                                ${this.data.symbol ? `<i class="fas ${this.data.symbol}"></i>`: ''}
+                            </h4>
                             ${this.data.custom?.tooltip?.recharge ? `<p class="notes"><i class="fa-solid fa-arrows-rotate" inert></i> <span>${this.data.custom.tooltip.recharge}</span></p>` : ''}
                         </div>
                         <div class="description">
@@ -57,6 +61,11 @@ export class FilterButton extends BG3Component {
                 // desc = this.data.custom?.tooltip ? `<div class="custom-tooltip dnd5e2">h4 style="--data-color:${this.data.color}">${this.data.custom?.tooltip?.label}</h4>${this.data.custom?.tooltip?.pills ? `<ul class="pills">${this.data.custom.tooltip.pills.map(p => `<li class="pill"><span class="label" style="color: #4e4e4e;">${p}</label></li>`).join('')}</ul>` : ''}</div>` : false;
                 // desc = this.data.custom?.tooltip ? `<div class="custom-tooltip dnd5e2"><h4 style="--data-color:${this.data.color}">${this.data.custom?.tooltip?.label}</h4><p class="notes"><i>${this.data.custom?.tooltip?.recharge}</i></p></div>` : false;
                 break;
+        }
+        if (desc) {
+            const controlHints = true //game.settings.get("dnd5e", "controlHints");
+            desc += controlHints ? '<p class="notes"><i>Left-click to highlight items using this slot.</i></p><p class="notes"><i>Right-click to grey out.</i></p>' : '';
+            desc = `<div class="custom-tooltip">${desc}</div>`;
         }
         return {type: 'simple', content: desc};
     }
